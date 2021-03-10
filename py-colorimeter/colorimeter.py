004f4e649b9fcc52274fa147bd8382c08d28a2c3
Joakim Skjefstad <skjefstad.joakim@gmail.com>
2021-03-10 15:01:37 +0100

    skeleton folders added

---
+++ py-colorimeter/colorimeter.py
@@ -0,0 +1,14 @@
+#!#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# colorimeter.py
+# description : see colors in at least one color
+# created by : Joakim Skjefstad
+# =============================================================================
+
+def main():
+    print("colorimeter-software says hello")
+    
+
+if __name__ == "__main__":
+    main()
